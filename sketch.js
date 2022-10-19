d5269e43416ecbfa291671072d3f9802f1c4c2ff
NOOBACON <113057576+NOOBACON@users.noreply.github.com>
2022-10-19 15:15:32 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,117 @@
+  
+var towerImg, tower;
+var doorImg, door, doorsGroup;
+var climberImg, climber, climbersGroup;
+var ghost, ghostImg;
+var invisibleBlockGroup, invisibleBlock;
+var gameState = "play"
+
+
+function preload(){
+  towerImg = loadImage("tower.png");
+  doorImg = loadImage("door.png");
+  climberImg = loadImage("climber.png");
+  ghostImg = loadImage("ghost-standing.png");
+  spookySound = loadSound("spooky.wav");
+}
+
+function setup() {
+  createCanvas(600,600);
+ //spookySound.loop(); eita som alto
+  tower = createSprite(300,300);
+  tower.addImage("tower",towerImg);
+  tower.velocityY = 1;
+  
+  doorsGroup = new Group();
+  climbersGroup = new Group();
+  invisibleBlockGroup = new Group();
+  
+  ghost = createSprite(400,200,50,50);
+  ghost.scale = 0.3;
+  ghost.addImage("ghost", ghostImg);
+}
+
+
+function draw() {
+  background(255);
+ if(tower.y > 300){
+      tower.y = 300
+    } 
+  
+  if (gameState === "play") {
+    
+    ghost.velocityX=(mouseX-ghost.x)/5;
+    if(keyDown("space")){
+  
+         ghost.velocityY = -10;
+
+      // escreva o código para mover para cima quando a tecla espaço for pressionada
+      
+    }
+  
+  ghost.velocityY = ghost.velocityY + 0.6;
+  
+   
+      //escreva uma condição para a torre de rolagem infinita
+    
+      spawnDoors();
+
+  
+//escreva um código para fazer invisibleBlockGroup (grupo de bloco invisível) colidir com o fantasma, destruir o fantasma e mudar o estado do jogo para end.
+     if(climbersGroup.isTouching(ghost)){
+      ghost.velocityY = 0;
+    }
+    if(invisibleBlockGroup.isTouching(ghost) || ghost.y > 600){
+      ghost.destroy();
+      gameState = "end";
+    }
+    
+  
+  drawSprites();
+}
+  if (gameState === "end"){
+    stroke("yellow");
+    fill("yellow");
+    textSize(30);
+    text("Game Over", 230,250);
+  }
+}
+
+function spawnDoors()
+ {
+  //escreva o código aqui para gerar as nuvens
+  if (frameCount % 240 === 0) {
+    var door = createSprite(Math.round(random(100, 500)), -50);
+    var climber = createSprite(door.x,10);
+    var invisibleBlock = createSprite(door.x,15);
+    invisibleBlock.width = climber.width;
+    invisibleBlock.height = 2;
+    //adicione a função aleatória
+    //
+    door.addImage(doorImg);
+    climber.addImage(climberImg);
+    
+    door.velocityY = 3;
+    climber.velocityY = 3;
+    invisibleBlock.velocityY = 3;
+
+    //mude a profundidade do fantasma e da porta
+    
+     
+    ghost.depth = door.depth;
+    ghost.depth +=1;
+    
+    //atribuir tempo de vida para a porta, escalador e bloco invisível
+
+    ghost.lifetime = 800;
+    ghost.lifetime = 800;
+    ghost.lifetime = 800;
+    //adicione cada obstáculo ao grupo obstaclesGroup.add(obstacle); aqui os obstáculos são as portas, o escalador e o bloco invisível
+    
+    doorsGroup.add(door);
+    invisibleBlock.debug = true;
+    climbersGroup.add(ghost);
+    invisibleBlockGroup.add(invisibleBlock);
+  }
+}
+
